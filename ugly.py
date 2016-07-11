adf3b619b2cfeeadc9351bfab941c4eea8216b7e
ModalSeoul <Algernon@teknik.io>
2016-07-11 14:54:08 -0600

    Now pulls from embed-ssl.wistia as well.

--- ugly.py
+++ ugly.py
@@ -22,38 +22,47 @@ def login():
     r = s.post('https://egghead.io/users/sign_in', data=DATA, headers=headers)
 
 
+def parse_bin(content, key):
+    print(key)
+    try:
+        file = content.split(key)[1].split('" />')[0]
+        bin_list.append('{}{}'.format(key, file))
+    except IndexError:
+        print('Failed on parse_bin')
+
+
 def build_list():
+    SSL_WISTIA = 'https://embed-ssl.wistia.com/deliveries/'
+    WISTIA = 'http://embed.wistia.com/deliveries/'
     r = s.get('https://egghead.io/technologies/{}'.format(argv[1]))
     tree = html.fromstring(r.text)
     items = tree.xpath('//a[@class=""]')
-    for item in items:
-        for i in item.items():
+    for item in range(0, len(items)):
+        for i in items[item].items():
             if 'https://egg' in i[1]:
                 t = s.get(i[1])
-
-                find_this = 'http://embed.wistia.com/deliveries/'
                 try:
-                    file = t.text.split(find_this)[1].split('" />')[0]
-                    # print(file)
-                    bin_list.append(
-                        'http://embed.wistia.com/deliveries/{}\n'.format(file))
+                    file = t.text.split(WISTIA)[1].split('" />')[0]
+                    bin_list.append('{}{}'.format(WISTIA, file))
                 except IndexError:
-                    print(bin_list)
-                    download_list()
-                    return
-                download_list()
+                    ssl_bin = t.text.split(SSL_WISTIA)[1].split('" />')[0]
+                    bin_list.append('{}{}'.format(SSL_WISTIA, ssl_bin))
+                    print('WE BROKE')
+                    print(i)
+                    item += 1
+    download_list()
 
 
 def download_bin(bin):
     file_name = bin.split('.bin')[0].split('deliveries/')[1]
-    with open('videos/{}/{}.mp4'.format(argv[1], file_name), 'wb') as video:
-        resp = requests.get(bin, stream=True)
+    video = open('videos/{}/{}.mp4'.format(argv[1], file_name), 'wb')
+    resp = requests.get(bin, stream=True)
 
-        if not resp.ok:
-            print('Request to the .bin file failed. Attempted url was %' % bin)
+    if not resp.ok:
+        print('Request to the .bin file failed. Attempted url was %' % bin)
 
-        for data in resp.iter_content(1024):
-            video.write(data)
+    for data in resp.iter_content(1024):
+        video.write(data)
 
 
 def download_list():
